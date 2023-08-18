d052b42da99f00f422410258405444c7584285a0
Tathagata Mishra <tathagatamishra007@gmail.com>
2023-08-18 20:42:51 +0530

    C1 done - profile, cover, Name, Tag, Description

--- client/src/Component/C1/About.jsx
+++ client/src/Component/C1/About.jsx
@@ -10,24 +10,35 @@ export default function About() {
 
       <div className="about-card">
 
-        <div className="cover-image">
-          <img src={coverImage} />
-        </div>
-        
         <div className="profile-image">
 
-          {/* <img className="user-image" src={profileImage} /> */}
+          <img className="user-image" src={profileImage} />
 
           <div className="bg-image">
             <img className="default-image" src={defaultProfile} />
           </div>
         </div>
+        
+        <div className="cover-image">
+          <div className="cover-shadow"></div>
+          <img src={coverImage} />
+        </div>
 
         <div className="bio">
           <div className="bio-holder">
+            <h1 className="bio-name">Aniruddha Sen</h1>
 
-          <h1></h1>
-          <p></p>
+            <div className="bio-info">
+              <p className="bio-topics">Bioinformatics Specialist</p>
+              
+              <div className="bio-tags">
+                <div className="tag">Transcridivtomics</div>
+                <div className="tag">Genomics</div>
+                <div className="tag">Metagenomics</div>
+              </div>
+            </div>
+
+            <p className="bio-description">Specializes in the study of the chemical processes and molecular mechanisms that underlie the structure, function, and regulation of biological molecules within living organisms. These researchers investigate the interactions, transformations, and reactions of biomolecules such as proteins, nucleic acids, lipids, carbohydrates, and small molecules, with the goal of understanding the fundamental principles governing life at the molecular level.</p>
           </div>
         </div>
       </div>
